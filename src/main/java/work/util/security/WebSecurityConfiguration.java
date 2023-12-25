2be25812258e12c7fa673ba732eb5b620f9a458d
Andrei Alesik <andrei.alesik@pcgacademia.pl>
2023-12-25 14:32:31 +0100

    change cors policy

--- src/main/java/work/util/security/WebSecurityConfiguration.java
+++ src/main/java/work/util/security/WebSecurityConfiguration.java
@@ -47,6 +47,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/swagger-ui.html").permitAll()
